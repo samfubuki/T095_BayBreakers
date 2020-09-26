b59f1283059002e9f265c1ac05c2e0961bdee479
samfubuki <pbhardwaj.preet@gmail.com>
2020-09-27 00:13:28 +0530

    Made the url to display the algo applied to message and key

--- decryptpart/urls.py
+++ decryptpart/urls.py
@@ -4,4 +4,5 @@ from . import views
 app_name='decryptpart'
 urlpatterns = [
     path('',views.enterform,name="decrypthome"),
+    path('outputform',views.output,name="outputform")
 ]
--- decryptpart/views.py
+++ decryptpart/views.py
@@ -3,3 +3,11 @@ from django.shortcuts import render
 # Create your views here.
 def enterform(request):
     return render(request, 'decryptpart/enterform.html')
+
+def output(request):
+    var1 = request.POST["key"]
+    print(var1)
+    var2 = request.POST["message"]
+    print(var2)
+    var3=var1+var2
+    return render(request, "decryptpart/outputform.html",  {'output':var3}) 
