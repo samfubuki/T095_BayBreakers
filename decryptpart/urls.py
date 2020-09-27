0602dcb2ca402b62da8bb41317fc3a48b438b6e1
samfubuki <pbhardwaj.preet@gmail.com>
2020-09-27 08:55:48 +0530

    The message code is running

--- decryptpart/urls.py
+++ decryptpart/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name='decryptpart'
 urlpatterns = [
     path('',views.loginpage,name="loginpage"),
+    path('enterform',views.enterform,name="enterform"),
     path('outputform',views.output,name="outputform")
 ]
--- decryptpart/views.py
+++ decryptpart/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib import messages
 
 # Create your views here.
 
@@ -12,86 +13,100 @@ def login(request):
     if request.method=='POST':
         #check if a user exists
         #check if password exists
-        uname=request.POST['username']
-        pwd = request.POST['password']
+        var uname=request.POST['username']
+        var pwd = request.POST['password']
+        print(uname)
         if uname=="General@123" and  pwd=="INDIA":
+           # messages.success(request, "You have been able to login successfully")
             return render(request, "decrypt/enterform.html")
+           
         else:
-            ret    
+            messages.error(request,'username or password not correct')
+            return redirect(request,'decryptpart/login.html')   
+           
 
 
 def output(request):
-    s = request.POST["key"]
-    key = request.POST["message"]
-    s = input().strip()
-    key = input().strip()
-    def decode(s,key):
-        s
-        n = len(s)
-        m = len(key)
-        t = key
-        n //=m
-        mat = [[0]*(m+1) for i in range(n+1)]
+    key = request.POST["key"]
+    s = request.POST["message"]
+    s = s.strip()
+    key = key.strip()
 
-        cnt = 0
-        for i in range(1,m+1):
-            for j in range(1,n+1):
-                if ord(s[cnt])==32:
-                    mat[j][i] = "0"
-                else:
-                    mat[j][i] = s[cnt]
-                    cnt+=1
-        key = sorted(list(key))
-        cnt = 0
-        for i in range(1,m+1):
-            mat[0][i] = key[cnt]
+    # print(s,key)
+    #def decode(s,key):
+    n = len(s)
+    m = len(key)
+    t = key
+    n //=m
+    mat = [["0"]*(m+1) for i in range(n+1)]
+
+    cnt = 0
+    for i in range(1,m+1):
+        for j in range(1,n+1):
+            if ord(s[cnt])==32:
+                mat[j][i] = "0"
+            else:
+                mat[j][i] = s[cnt]
             cnt+=1
-        key = t
-        matrix = [[0]*(m+1) for i in range(n+1)]
-        cnt = 1
-        for i in range(m):
-            for j in range(1,m+1):
-                if key[i]==mat[0][j]:
-                    for k in range(1,n+1):
-                        matrix[k][cnt]=mat[k][j]
-                    cnt+=1
-        v = []
-        cnt = 0
-        a = ""
-        for i in range(1,n+1):
-            for j in range(1,m+1):
-                if matrix[i][j]=="0" or matrix[i][j]=="_":
-                    v.append(a)
-                    a = ""
-                a+=matrix[i][j]
-        mp = {}
-        mp["Khardung"]=1
-        mp["Lachulung"]=1
-        mp["Gyong"]=1
-        mp["Sasser"]=1
-        mp["Zoji"]=1
-        mp["Sia"]=1
-        mp["Indira"]=1
-        mp["Rezang"]=1
-        mp["Tanglang"]=1
-        mp["Pensi"]=1
-        mp["Marsimik"]=1
-        flag = 0
-        for i in range(len(v)):
-            if v[i]=="not" or v[i]=="not":
-                flag = 1
-        for i in range(len(v)):
-            if v[i] in mp:
-                mp[v[i]] = 0
-        res = []
-        if flag ==1:
-            for key,val in mp:
-                if val==1:
-                    res.append(key)
-        else:
-            for key,val in mp.items():
-                if val==0:
-                    res.append(key)
-    decode(s,key)
+    key = sorted(list(key))
+    cnt = 0
+    for i in range(1,m+1):
+        mat[0][i] = key[cnt]
+        cnt+=1
+    key = t
+    # print(mat)
+    matrix = [[0]*(m+1) for i in range(n+1)]
+    cnt = 1
+    for i in range(m):
+        for j in range(1,m+1):
+            if key[i]==mat[0][j]:
+                for k in range(1,n+1):
+                    matrix[k][cnt]=mat[k][j]
+                cnt+=1
+    v = []
 
-    ##return render(request, "decryptpart/outputform.html",  {'output':}) 
+    cnt = 0
+    a = ""
+    for i in range(1,n+1):
+        for j in range(1,m+1):
+            if matrix[i][j]=="0" or matrix[i][j]=="_":
+                v.append(a)
+                a = ""
+                continue
+            a+=matrix[i][j]
+    print(v)        
+    mp = {}
+    mp["Khardung"]=1
+    mp["Lachulung"]=1
+    mp["Gyong"]=1
+    mp["Sasser"]=1
+    mp["Zoji"]=1
+    mp["Sia"]=1
+    mp["Indira"]=1
+    mp["Rezang"]=1
+    mp["Tanglang"]=1
+    mp["Pensi"]=1
+    mp["Marsimik"]=1
+    flag = 0
+    for i in range(len(v)):
+        if v[i] in ["not", "Not"]:
+            flag = 1
+    for i in range(len(v)):
+        if v[i] in mp:
+            mp[v[i]] = 0
+    res = []
+    # print(mp)
+    if flag ==1:
+        for key in sorted(mp.keys()):
+            if mp[key] ==1:
+                res.append(key)
+    else:
+        for key in sorted(mp.keys()):
+            if mp[key]==0:
+                res.append(key)
+    x = " ".join(res) 
+    print(x)
+    v = " ".join(v)
+    #x = decode(s,key)         
+
+    return render(request, "decryptpart/outputform.html",  {'output':x,'message':v}) 
